8aa09ba948be1219f3bb63c411d7fb0c7f40a6b9
MINSK\Siarhei_Mokin <siarhei_mokin@epam.com>
2019-12-18 17:39:56 +0300

    initial

--- src/ListPool/ListPool.cs
+++ src/ListPool/ListPool.cs
@@ -7,69 +7,237 @@ using System.Runtime.CompilerServices;
 
 namespace ListPool
 {
-    public struct ListPool<TSource> : IDisposable, IValueEnumerable<TSource>
+    public struct ListPool<TSource> : IDisposable, IValueEnumerable<TSource>, IList<TSource>
     {
-        public readonly TSource this[int index]
+        private readonly TSource[] _buffer;
+        private int _itemsCount;
+        private int _size;
+
+        public readonly int Count => _itemsCount;
+        public readonly bool IsReadOnly { get; }
+
+        public void Add(TSource item)
         {
-            get
+            if (IsReadOnly)
             {
-                if (index >= Length)
-                {
-                    throw new Exception("Array overflow");
-                }
-
-                return index >= itemsCount ? default : buffer[index];
+                throw new InvalidOperationException("ListPool is readonly.");
             }
-        }
 
-        public readonly int Length;
-
-        private readonly TSource[] buffer;
-        private int itemsCount;
-
-        private ListPool(in int length)
-        {
-            buffer = ArrayPool<TSource>.Shared.Rent(length);
-            Length = length;
-            itemsCount = 0;
-        }
-
-        public static ListPool<TSource> Rent(in int length)
-        {
-            return new ListPool<TSource>(in length);
-        }
-
-        public void Add(in TSource item)
-        {
-            if (itemsCount >= Length)
+            if (_itemsCount >= _size)
             {
                 throw new Exception("Array overflow");
             }
 
-            buffer[itemsCount] = item;
-            itemsCount++;
+            _buffer[_itemsCount] = item;
+            _itemsCount++;
         }
 
-        public void Dispose()
+        public void Clear()
         {
-            ArrayPool<TSource>.Shared.Return(buffer);
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("ListPool is readonly.");
+            }
+
+            _itemsCount = 0;
+            for (var i = 0; i < _itemsCount; i++)
+            {
+                _buffer[i] = default;
+            }
+        }
+
+        public readonly bool Contains(TSource item)
+        {
+            if (item == null)
+            {
+                for (var i = 0; i < _itemsCount; i++)
+                {
+                    if (_buffer[i] == null) return true;
+                }
+
+                return false;
+            }
+
+            for (var i = 0; i < _itemsCount; i++)
+            {
+                if (_buffer[i] == null) continue;
+
+                if (_buffer[i].Equals(item)) return true;
+            }
+
+            return false;
+        }
+
+        public readonly void CopyTo(TSource[] array, int arrayIndex)
+        {
+            Array.Copy(_buffer, 0, array, arrayIndex, _itemsCount);
+        }
+
+        public bool Remove(TSource item)
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("ListPool is readonly.");
+            }
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            var index = IndexOf(item);
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(index);
+
+            return true;
+        }
+
+        public readonly int IndexOf(TSource item)
+        {
+            if (item == null)
+            {
+                return -1;
+            }
+
+            for (var i = 0; i < _itemsCount; i++)
+            {
+                if(_buffer[i] == null) continue;
+
+                if (_buffer[i].Equals(item)) return i;
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, TSource item)
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("ListPool is readonly.");
+            }
+
+            if (index < 0 || index >= _size) throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (index == _size - 1)
+            {
+                if (_buffer[index] != null)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                _itemsCount++;
+                _buffer[index] = item;
+                return;
+            }
+
+            _itemsCount++;
+
+            if (index >= _itemsCount)
+            {
+                _buffer[index] = item;
+            }
+
+            var save = _buffer[index];
+
+            _buffer[index] = item;
+
+            for (var i = ++index; i < _size; i++)
+            {
+                var swap = _buffer[i];
+                _buffer[i] = save;
+                save = swap;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("ListPool is readonly.");
+            }
+
+            if (index < 0 || index >= _size) throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (index >= _itemsCount)
+            {
+                return;
+            }
+
+            _itemsCount--;
+
+            for (var i = index; i < _itemsCount; i++)
+            {
+                _buffer[i] = _buffer[i + 1];
+            }
+        }
+
+        private ListPool(in int size, in bool isReadOnly)
+        {
+            _buffer = ArrayPool<TSource>.Shared.Rent(size);
+            _itemsCount = 0;
+
+            _size = size;
+            IsReadOnly = isReadOnly;
+        }
+
+        public static ListPool<TSource> Rent(in int length, bool isReadOnly = false)
+        {
+            return new ListPool<TSource>(in length, isReadOnly);
+        }
+
+        public readonly TSource this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new IndexOutOfRangeException(nameof(index));
+                }
+
+                return index >= _itemsCount ? default : _buffer[index];
+            }
+
+            set
+            {
+                if (IsReadOnly)
+                {
+                    throw new InvalidOperationException("ListPool is readonly.");
+                }
+
+                if (index < 0 || index >= _size)
+                {
+                    throw new IndexOutOfRangeException(nameof(index));
+                }
+
+                _buffer[index] = value;
+            }
         }
 
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public readonly Enumerator<TSource> GetEnumerator()
         {
-            return new Enumerator<TSource>(in buffer, in itemsCount);
+            return new Enumerator<TSource>(in _buffer, in _itemsCount);
         }
 
         readonly IEnumerator<TSource> IEnumerable<TSource>.GetEnumerator()
         {
-            return new Enumerator<TSource>(in buffer, in itemsCount);
+            return new Enumerator<TSource>(in _buffer, in _itemsCount);
         }
 
         readonly IEnumerator IEnumerable.GetEnumerator()
         {
-            return new Enumerator<TSource>(in buffer, in itemsCount);
+            return new Enumerator<TSource>(in _buffer, in _itemsCount);
+        }
+
+        public readonly void Dispose()
+        {
+            ArrayPool<TSource>.Shared.Return(_buffer);
         }
     }
 }
--- tests/ListPool.UnitTests/ListPoolTests.cs
+++ tests/ListPool.UnitTests/ListPoolTests.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Xunit;
 
 namespace ListPool.UnitTests
@@ -6,11 +5,11 @@ namespace ListPool.UnitTests
     public class ListPoolTests
     {
         [Fact]
-        public void Create_list_and_AddValues()
+        public void Create_list_and_add_values()
         {
-            int expectedAtFirst = 5;
-            int expectedAtSecond = 7;
-            int expectedAtThird = 10;
+            const int expectedAtFirst = 5;
+            const int expectedAtSecond = 7;
+            const int expectedAtThird = 10;
 
             using var sut = ListPool<int>.Rent(3);
             sut.Add(expectedAtFirst);
@@ -25,15 +24,60 @@ namespace ListPool.UnitTests
         [Fact]
         public void Enumerate_added_items_and_ignore_others()
         {
-            int listCapacity = 10;
-            int expectedCount = 3;
+            const int listCapacity = 10;
+            const int expectedCount = 3;
 
             using var sut = ListPool<int>.Rent(listCapacity);
             sut.Add(1);
             sut.Add(2);
             sut.Add(3);
 
-            Assert.Equal(expectedCount, sut.Count());
+            Assert.Equal(expectedCount, sut.Count);
+        }
+
+        [Fact]
+        public void Create_list_and_add_values_after_remove()
+        {
+            const int expectedAtFirst = 5;
+            const int expectedAtSecond = 7;
+            const int expectedAtThird = 10;
+            const int expectedCountAfterRemove = 2;
+
+            using var sut = ListPool<int>.Rent(3);
+            sut.Add(expectedAtFirst);
+            sut.Add(expectedAtSecond);
+            sut.Add(expectedAtThird);
+
+            Assert.Equal(expectedAtFirst, sut[0]);
+            Assert.Equal(expectedAtSecond, sut[1]);
+            Assert.Equal(expectedAtThird, sut[2]);
+
+            Assert.True(sut.Remove(expectedAtFirst));
+            Assert.Equal(expectedCountAfterRemove, sut.Count);
+        }
+
+        [Fact]
+        public void Create_list_and_add_values_after_remove_by_index()
+        {
+            const int expectedAtFirst = 5;
+            const int expectedAtSecond = 7;
+            const int expectedAtThird = 10;
+            const int expectedCountAfterRemove = 2;
+
+            using var sut = ListPool<int>.Rent(3);
+            sut.Add(expectedAtFirst);
+            sut.Add(expectedAtSecond);
+            sut.Add(expectedAtThird);
+
+            Assert.Equal(expectedAtFirst, sut[0]);
+            Assert.Equal(expectedAtSecond, sut[1]);
+            Assert.Equal(expectedAtThird, sut[2]);
+
+            sut.RemoveAt(1);
+            var actualResult = sut.Contains(expectedAtSecond);
+
+            Assert.False(actualResult);
+            Assert.Equal(expectedCountAfterRemove, sut.Count);
         }
     }
 }
