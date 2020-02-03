24f5bbd1881ff99570a459a0639249871ec5f95c
Fausto David <fsuarez@travix.com>
2020-02-03 11:38:27 +0100

    Add Serialize benchmark, format ListPool, make benchmkar always running

---
+++ perf/ListPool.Benchmarks/Utf8JsonSerializeListOfIntBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Utf8Json;
+
+namespace ListPool.Benchmarks
+{
+    [RPlotExporter]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MemoryDiagnoser]
+    [GcServer(true)]
+    [GcConcurrent]
+    public class Utf8JsonSerializeListOfIntBenchmarks
+    {
+        private List<int> _list;
+        private ListPool<int> _listPool;
+
+        [Params(10000)]
+        public int N { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var items = Enumerable.Range(0, N).ToArray();
+            _listPool = items.ToListPool();
+            _list = items.ToList();
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _listPool.Dispose();
+        }
+
+        [Benchmark(Baseline = true)]
+        public int List()
+        {
+            string serializedItems = JsonSerializer.ToJsonString(_list);
+            return serializedItems.Length;
+        }
+
+        [Benchmark]
+        public int ListPool()
+        {
+            string serializedItems = JsonSerializer.ToJsonString(_listPool);
+            return serializedItems.Length;
+        }
+    }
+}
