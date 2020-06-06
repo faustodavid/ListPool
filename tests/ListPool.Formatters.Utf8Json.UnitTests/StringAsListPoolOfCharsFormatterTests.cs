bd38af494304d80fb1ac6ac49dc062b34ba69225
Fausto David Suarez Rosario <faustodavid@hotmail.com>
2020-06-07 00:03:33 +0200

    add test project file

---
+++ tests/ListPool.Formatters.Utf8Json.UnitTests/StringAsListPoolOfCharsFormatterTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Utf8Json;
+using Xunit;
+
+namespace ListPool.Formatters.Utf8Json.UnitTests
+{
+    public class StringAsListPoolOfCharsFormatterTests
+    {
+        public class DummyClassUsingListPool : IDisposable
+        {
+            [JsonFormatter(typeof(StringAsListPoolOfCharsFormatter))]
+            public ListPool<char> Text { get; set; }
+
+            public void Dispose() => Text?.Dispose();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var dummyClass = new DummyClassUsingListPool();
+            dummyClass.Text = new ListPool<char>();
+            dummyClass.Text.AddRange(stackalloc char[4] {'h', 'o', 'l', 'a'});
+
+            string serialized = JsonSerializer.ToJsonString(dummyClass);
+
+            Assert.Equal("{\"Text\":\"hola\"}", serialized);
+        }
+    }
+}
