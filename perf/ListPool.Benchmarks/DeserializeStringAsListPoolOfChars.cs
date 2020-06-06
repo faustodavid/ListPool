0937d00a7042fabb95c7a9993c89cedd4f763485
Fausto David Suarez Rosario <faustodavid@hotmail.com>
2020-06-07 00:15:43 +0200

    add benchmark for serialization

--- perf/ListPool.Benchmarks/DeserializeStringAsListPoolOfChars.cs
+++ perf/ListPool.Benchmarks/DeserializeStringAsListPoolOfChars.cs
@@ -7,6 +7,55 @@ using Utf8Json;
 namespace ListPool.Benchmarks
 {
     [MemoryDiagnoser]
+    public class SerializeStringAsListPoolOfChars
+    {
+        [Params(500, 1000)] public int N { get; set; }
+
+        private DummyClass _dummyClass = new DummyClass();
+        private DummyClassUsingListPool _dummyClassUsingListPool = new DummyClassUsingListPool();
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            StringBuilder sb = new StringBuilder(N);
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append("a");
+            }
+
+            _dummyClass.Text = sb.ToString();
+            _dummyClassUsingListPool.Text?.Dispose();
+            _dummyClassUsingListPool.Text = new ListPool<char>(_dummyClass.Text.ToCharArray());
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _dummyClassUsingListPool.Text?.Dispose();
+            _dummyClassUsingListPool.Text = null;
+        }
+
+        [Benchmark(Baseline = true)]
+        public byte[] UsingString() => JsonSerializer.Serialize(_dummyClass);
+
+        [Benchmark]
+        public byte[] UsingListPool() => JsonSerializer.Serialize(_dummyClassUsingListPool);
+    }
+
+    public class DummyClass
+    {
+        public string Text { get; set; }
+    }
+
+    public class DummyClassUsingListPool : IDisposable
+    {
+        [JsonFormatter(typeof(StringAsListPoolOfCharsFormatter))]
+        public ListPool<char> Text { get; set; }
+
+        public void Dispose() => Text?.Dispose();
+    }
+
+    [MemoryDiagnoser]
     public class DeserializeStringAsListPoolOfChars
     {
         private byte[] _json;
@@ -36,19 +85,6 @@ namespace ListPool.Benchmarks
         {
             using var dummyClass = JsonSerializer.Deserialize<DummyClassUsingListPool>(_json);
             return dummyClass.Text.Count;
-        }
-
-        public class DummyClass
-        {
-            public string Text { get; set; }
-        }
-
-        public class DummyClassUsingListPool : IDisposable
-        {
-            [JsonFormatter(typeof(StringAsListPoolOfCharsFormatter))]
-            public ListPool<char> Text { get; set; }
-
-            public void Dispose() => Text?.Dispose();
         }
     }
 }
