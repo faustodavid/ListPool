225397d3d3fbc4230df54a85ee08fc0e00ef12d5
Fausto David Suarez Rosario <faustodavid@hotmail.com>
2020-06-14 12:25:03 +0200

    include file

---
+++ src/ListPool.Serializers.SystemTextJson.Converters/StringAsListPoolOfCharsConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ListPool.Serializers.System.Text.Json.Converters
+{
+    public sealed class StringAsListPoolOfCharsConverter : JsonConverter<ListPool<char>>
+    {
+        public override ListPool<char> Read(ref Utf8JsonReader reader, Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            ReadOnlySpan<byte> writtenBytes = reader.ValueSpan;
+            ListPool<char> listPool = new ListPool<char>(Encoding.UTF8.GetCharCount(writtenBytes));
+
+            int charsCount = Encoding.UTF8.GetChars(writtenBytes, listPool.GetRawBuffer());
+            listPool.SetOffsetManually(charsCount);
+
+            return listPool;
+        }
+
+        public override void Write(Utf8JsonWriter writer, ListPool<char> value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.AsSpan());
+        }
+    }
+}
